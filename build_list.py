840c9519dc27b1982db2393edc19ea54f45b8605
anyin233 <shizukuprprpr@gmail.com>
2024-12-23 15:04:37 +0800

    refactor code for consistency and readability; adjust indentation and spacing

--- build_list.py
+++ build_list.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup
-from tqdm import tqdm
 from tqdm.auto import trange
 import requests
 import os
@@ -8,28 +7,30 @@ from config import *
 
 
 def parse_page_number(soup):
-  strings = soup.find_all("div", class_="paging")[0].strings
-  page_string = list(strings)[0]
-  num_papers = int(''.join(filter(str.isdigit, page_string)))
-  return num_papers
+    strings = soup.find_all("div", class_="paging")[0].strings
+    page_string = list(strings)[0]
+    num_papers = int("".join(filter(str.isdigit, page_string)))
+    return num_papers
+
 
 def get_arxiv_ids(soup):
-  id_nodes = soup.find_all("a", title="Abstract")
-  ids = [node["id"] for node in id_nodes]
-  title_nodes = soup.find_all("div", class_="list-title mathjax")
-  titles = [list(node.strings)[1].strip() for node in title_nodes]
-  id_dict = {}
-  for id, title in zip(ids, titles):
-    id_dict[id] = title
-  return id_dict
+    id_nodes = soup.find_all("a", title="Abstract")
+    ids = [node["id"] for node in id_nodes]
+    title_nodes = soup.find_all("div", class_="list-title mathjax")
+    titles = [list(node.strings)[1].strip() for node in title_nodes]
+    id_dict = {}
+    for id, title in zip(ids, titles):
+        id_dict[id] = title
+    return id_dict
 
-pbar_year = trange(int(START_YEAR), int(END_YEAR)+1)
+
+pbar_year = trange(int(START_YEAR), int(END_YEAR) + 1)
 
 for year in pbar_year:
     pbar_year.set_description(f"Processing {year}")
     url = f"{BASE_URL}/list/{FIELD}/{year}"
     pbar_outer = trange(1, 13)
-    
+
     for m in pbar_outer:
         murl = f"{url}-{m:02d}"
         pbar_outer.set_description(f"Processing {year}-{m:02d}")
@@ -42,15 +43,15 @@ for year in pbar_year:
         paper_of_this_month = []
         pbar_inner = trange(0, num_papers, 2000)
         for skip_id in pbar_inner:
-          skurl = f"{murl}?skip={skip_id}&show=2000"
-          response = requests.get(skurl)
-          soup = BeautifulSoup(response.content, "lxml-xml")
-          ids = get_arxiv_ids(soup)
-          paper_of_this_month.extend(ids.items())
-          if len(ids) == 0:
-            print("No more papers")
+            skurl = f"{murl}?skip={skip_id}&show=2000"
+            response = requests.get(skurl)
+            soup = BeautifulSoup(response.content, "lxml-xml")
+            ids = get_arxiv_ids(soup)
+            paper_of_this_month.extend(ids.items())
+            if len(ids) == 0:
+                print("No more papers")
         # String into csv file
         with open(os.path.join(LIST_PATH, f"{year}-{m:02d}.csv"), "w") as f:
-          f.write("paper_id,title\n")
-          for paper_id, title in paper_of_this_month:
-            f.write(f"{paper_id},\"{title}\"\n")
+            f.write("paper_id,title\n")
+            for paper_id, title in paper_of_this_month:
+                f.write(f'{paper_id},"{title}"\n')
--- download_paper.py
+++ download_paper.py
@@ -4,64 +4,76 @@ import requests
 import logging
 import os
 
+from tqdm import tqdm
+
 import re
 
 # Configure logging
 logging.basicConfig(
-  filename=LOG_PATH,
-  format='%(asctime)s - %(levelname)s - %(message)s'
+    filename=LOG_PATH, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
 logger = logging.getLogger(__name__)
 
+
 def save_pdf(paper_id, save_path):
-  try:
-    response = requests.get(f"{DOWNLOAD_API}/pdf/{paper_id}")
-    if response.status_code == 200:
-      with open(f"{save_path}/{paper_id}/{paper_id}.pdf", 'wb') as f:
-        f.write(response.content)
-    else:
-      logger.warning(f"Failed to download {paper_id}, status code: {response.status_code}")
-  except Exception as e:
-    logger.error(f"Error downloading {paper_id}: {str(e)}")
-    
+    try:
+        response = requests.get(f"{DOWNLOAD_API}/pdf/{paper_id}")
+        if response.status_code == 200:
+            with open(f"{save_path}/{paper_id}/{paper_id}.pdf", "wb") as f:
+                f.write(response.content)
+        else:
+            logger.warning(
+                f"Failed to download {paper_id}, status code: {response.status_code}"
+            )
+    except Exception as e:
+        logger.error(f"Error downloading {paper_id}: {str(e)}")
+
+
 def save_tex(paper_id, save_path):
-  try:
-    response = requests.get(f"{DOWNLOAD_API}/src/{paper_id}")
-    if response.status_code == 200:
-      filename = response.headers.get('content-disposition', '').split('filename=')[-1]
-      if not filename:
-        filename = f"{paper_id}.tar.gz"
-      with open(f"{save_path}/{paper_id}/{filename}", 'wb') as f:
-        f.write(response.content)
-    else:
-      logger.warning(f"Failed to download {paper_id}, status code: {response.status_code}")
-  except Exception as e:
-    logger.error(f"Error downloading {paper_id}: {str(e)}")
-    
-    
+    try:
+        response = requests.get(f"{DOWNLOAD_API}/src/{paper_id}")
+        if response.status_code == 200:
+            filename = response.headers.get("content-disposition", "").split(
+                "filename="
+            )[-1]
+            if not filename:
+                filename = f"{paper_id}.tar.gz"
+            with open(f"{save_path}/{paper_id}/{filename}", "wb") as f:
+                f.write(response.content)
+        else:
+            logger.warning(
+                f"Failed to download {paper_id}, status code: {response.status_code}"
+            )
+    except Exception as e:
+        logger.error(f"Error downloading {paper_id}: {str(e)}")
+
+
 def download_paper(paper_id):
-  if not os.path.exists(os.path.join(DOWNLOAD_PATH, paper_id)):
-    os.makedirs(os.path.join(DOWNLOAD_PATH, paper_id))
-  save_pdf(paper_id, DOWNLOAD_PATH)
-  save_tex(paper_id, DOWNLOAD_PATH)
-  
+    if not os.path.exists(os.path.join(DOWNLOAD_PATH, paper_id)):
+        os.makedirs(os.path.join(DOWNLOAD_PATH, paper_id))
+    save_pdf(paper_id, DOWNLOAD_PATH)
+    save_tex(paper_id, DOWNLOAD_PATH)
+
+
 def read_paper_list(path):
-  # traverse the path contains csv files
-  for root, dirs, files in os.walk(path):
-    for file in files:
-      if file.endswith(".csv"):
-        with open(os.path.join(root, file), "r") as f:
-          lines = f.readlines()
-          for line in lines[1:]:
-            paper_id = re.match(r'(\d+\.\d+)', line).group(1)
-            title = re.match(r'.*?"(.*?)"', line).group(1)
-            yield paper_id, title
-            
+    # traverse the path contains csv files
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(".csv"):
+                with open(os.path.join(root, file), "r") as f:
+                    lines = f.readlines()
+                    for line in lines[1:]:
+                        paper_id = re.match(r"(\d+\.\d+)", line).group(1)
+                        title = re.match(r'.*?"(.*?)"', line).group(1)
+                        yield paper_id, title
+
+
 def download_all_papers(path):
-  for paper_id, title in read_paper_list(path):
-    download_paper(paper_id)
-    logger.info(f"Downloaded {title}")
-    
+    for paper_id, title in tqdm(read_paper_list(path)):
+        download_paper(paper_id)
+        logger.info(f"Downloaded {title}")
+
+
 if __name__ == "__main__":
-  download_all_papers(LIST_PATH)
+    download_all_papers(LIST_PATH)
